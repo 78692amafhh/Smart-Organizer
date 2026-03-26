8abc77539004dc0f1da3518b40474c923a47c7a8
78692amafhh <78692amafhh@gmail.com>
2026-03-26 20:00:39 +0530

    Remove frontend from tracking

--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173   // या कोई भी free port
-  }
-});
